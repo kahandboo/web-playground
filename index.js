5862b0eb5d89fadc93258b85c953af6f0557fb65
감사감 <kokio02@pusan.ac.kr>
2025-12-25 13:47:40 +0900

    save post in memory

--- index.js
+++ index.js
@@ -11,3 +11,18 @@ app.listen(3000, () => {
     console.log('서버시작');
 });
 
+const posts = [];
+
+app.post('/post', (req, res) => {
+    const { title, content } = req.body;
+    posts.push({ title, content });
+    res.redirect('/posts');
+});
+
+app.get('/posts', (req, res) => {
+    let html = '<h1>게시글 목록</h1>';
+    posts.forEach(post => {
+        html += `<h2>${post.title}</h2><p>${post.content}</p>`;
+    });
+    res.send(html);
+});
