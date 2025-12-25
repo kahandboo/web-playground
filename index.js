75361f80aa52f44cc0fff855c4f3faf6f41d4640
감사감 <kokio02@pusan.ac.kr>
2025-12-25 16:42:00 +0900

    add login feature

--- index.js
+++ index.js
@@ -1,28 +1,55 @@
+const { render } = require('ejs');
 const express = require('express');
+const session = require('express-session');
 const app = express();
 
+app.set('view engine', 'ejs');
 app.use(express.urlencoded({ extended: true }));
-
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/views/index.html');
-});
 
 app.listen(3000, () => {
     console.log('서버시작');
 });
 
+app.use(session({
+    secret: 'mySecretKey', 
+    resave: false,
+    saveUninitialized: true,
+    cookie: { 
+        httpOnly: true,
+        secure: false
+    },
+    name: 'session-cookie'
+}));
+
+app.get('/', (req, res) => {
+    if (req.session.isLoggedIn){
+        res.sendFile(__dirname + '/views/index.html');
+    } else {
+        res.redirect('/login');
+    }
+});
+
 const posts = [];
+
+app.get('/login', (req, res) => {
+    res.sendFile(__dirname + '/views/login.html');
+});
+
+app.post('/login', (req, res) => {
+    const { username } = req.body;
+    req.session.userId = username;
+    req.session.isLoggedIn = true;
+    res.redirect('/');
+});
 
 app.post('/post', (req, res) => {
     const { title, content } = req.body;
-    posts.push({ title, content });
+    const username = req.session.userId;
+    posts.push({ title, content, username });
     res.redirect('/posts');
 });
 
 app.get('/posts', (req, res) => {
-    let html = '<h1>게시글 목록</h1>';
-    posts.forEach(post => {
-        html += `<h2>${post.title}</h2><p>${post.content}</p>`;
-    });
-    res.send(html);
+    res.render('posts', { posts: posts });
 });
+
